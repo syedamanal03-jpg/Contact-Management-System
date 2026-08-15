8a6bc02ca4731cb01d00b91fdc0759637c09d5de
Manal Abdullah <syedamanal03@gmail.com>
2026-08-15 10:55:07 +0500

    linkedlist added and gitignore updated

--- ContactManagementSystem/Contact.cs
+++ ContactManagementSystem/Contact.cs
@@ -1,1 +1,13 @@
-Console.Beep();
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact_Management_System
+{
+    internal class Contact
+    {
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ ContactManagementSystem/ContactNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact_Management_System
+{
+    internal class Contactnode
+    {
+        public Contact Data { get; set; } = new Contact();
+        public Contactnode? Next { get; set; } 
+    }
+}
